79efe9fc954bbdc38c7786fa48726c1c8f22b971
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-12 12:03:02 +0700

    Update callsmusic.py

--- EXmusic/services/callsmusic/callsmusic.py
+++ EXmusic/services/callsmusic/callsmusic.py
@@ -18,8 +18,8 @@
 from pyrogram import Client
 from pytgcalls import PyTgCalls
 
-from GeezProject.config import API_HASH, API_ID, SESSION_NAME
-from GeezProject.services.queues import queues
+from EXmusic.config import API_HASH, API_ID, SESSION_NAME
+from EXmusic.services.queues import queues
 
 client = Client(SESSION_NAME, API_ID, API_HASH)
 pytgcalls = PyTgCalls(client)
