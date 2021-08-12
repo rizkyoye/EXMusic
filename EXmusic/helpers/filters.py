43ec77ead1c936ffc665f7bd4d1eb68fdfe7946f
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-12 12:00:19 +0700

    Update filters.py

--- EXmusic/helpers/filters.py
+++ EXmusic/helpers/filters.py
@@ -19,7 +19,7 @@ from typing import List, Union
 
 from pyrogram import filters
 
-from GeezProject.config import COMMAND_PREFIXES
+from EXmusic.config import COMMAND_PREFIXES
 
 other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
 other_filters2 = (
