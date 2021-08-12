a31376b55abb8900d9211cfe61dc83b547d4b64c
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-12 19:20:42 +0700

    Update ytsearch.py

--- EXmusic/modules/ytsearch.py
+++ EXmusic/modules/ytsearch.py
@@ -1,5 +1,5 @@
 # Daisyxmusic (Telegram bot project )
-# Copyright (C) 2021  Inukaasith
+# Copyright (C) 2021  Riz-ex
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
