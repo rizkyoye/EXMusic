bd911382675a7494bf0dd9e36915c80e404e2a19
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-19 03:48:18 +0700

    Update play.py

--- EXmusic/modules/play.py
+++ EXmusic/modules/play.py
@@ -602,7 +602,7 @@ async def play(_, message: Message):
         ydl_opts = {"format": "bestaudio[ext=m4a]"}
         
         try:
-          results = YoutubeSearch(query, max_results=5).to_dict()
+          results = YoutubeSearch(query, max_results=6).to_dict()
         except:
           await lel.edit("**Give the title of the song to play!**")
         # Looks like hell. Aren't it?? FUCK OFF
@@ -611,8 +611,8 @@ async def play(_, message: Message):
             j = 0
             useer=user_name
 
-            emojilist = ["1️⃣","2️⃣","3️⃣","4️⃣","5️⃣"]
-            while j < 5:
+            emojilist = ["1️⃣","2️⃣","3️⃣","4️⃣","5️⃣","6️⃣"]
+            while j < 6:
                 toxxt += f"{emojilist[j]} [{results[j]['title'][:25]}](https://youtube.com{results[j]['url_suffix']})\n"
                 toxxt += f" ├ 💡 **Duration** - {results[j]['duration']}\n"
                 toxxt += f" └ ⚡ __Powered by EX MUSIC__\n\n"
@@ -628,10 +628,11 @@ async def play(_, message: Message):
                     [
                         InlineKeyboardButton("4️⃣", callback_data=f'plll 3|{query}|{user_id}'),
                         InlineKeyboardButton("5️⃣", callback_data=f'plll 4|{query}|{user_id}'),
+                        InlineKeyboardButton("6️⃣", callback_data=f'plll 5|{query}|{user_id}'),
                     ],
                     [
                         InlineKeyboardButton(text="🗑 ᴄʟᴏsᴇ", callback_data="cls"),
-                        InlineKeyboardButton(text="📣 ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/EXProjects")],
+                        InlineKeyboardButton(text="⏺️ ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/EXProjects")],
                 ]
             ) 
             await lel.edit(toxxt,reply_markup=koyboard,disable_web_page_preview=True)
