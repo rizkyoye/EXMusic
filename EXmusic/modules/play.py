9ae7be395f76352559c96b0ab35d69f1564b133f
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-18 19:52:40 +0700

    Update play.py

--- EXmusic/modules/play.py
+++ EXmusic/modules/play.py
@@ -634,10 +634,9 @@ async def play(_, message: Message):
                         InlineKeyboardButton(text="📣 ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/EXProjects")],
                 ]
             ) 
+            await lel.edit(toxxt,reply_markup=koyboard,disable_web_page_preview=True,reply_photo)
             await message.reply_photo(
                 photo="https://telegra.ph/file/c595250581bdbbe5be931.jpg",
-                caption=toxxt, reply_markup=koyboard,
-                disable_web_page_preview=True
             )
             await lel.delete()
             return
