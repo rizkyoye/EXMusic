d7f1b53539376cdc4ab31c437ac78b55a7fc40bb
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-19 19:44:12 +0700

    Update private.py

--- EXmusic/modules/private.py
+++ EXmusic/modules/private.py
@@ -56,10 +56,10 @@ async def _human_time_duration(seconds):
 async def start_(client: Client, message: Message):
     await message.reply_text(
         f"""<b>👋 **𝗪𝗲𝗹𝗰𝗼𝗺𝗲** {message.from_user.first_name}**\n
-💭 [𝗘𝗫 𝗠𝘂𝘀𝗶𝗰](https://t.me/{BOT_USERNAME}) allow you to play **music** on groups through the new **Telegram's** voice chat!
-💡 Click [𝗵𝗲𝗿𝗲](https://t.me/{BOT_USERNAME}?startgroup=true) to add me to your **group.**
+💭 [𝗘𝗫 𝗠𝘂𝘀𝗶𝗰](https://t.me/{BOT_USERNAME}) 𝗮𝗹𝗹𝗼𝘄 𝘆𝗼𝘂 𝘁𝗼 𝗽𝗹𝗮𝘆 𝗺𝘂𝘀𝗶𝗰 𝗼𝗻 𝗴𝗿𝗼𝘂𝗽𝘀 𝘁𝗵𝗿𝗼𝘂𝗴𝗵 𝘁𝗵𝗲 𝗻𝗲𝘄 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺'𝘀 𝘃𝗼𝗶𝗰𝗲 𝗰𝗵𝗮𝘁𝘀!
+💡 𝗖𝗹𝗶𝗰𝗸 [𝗵𝗲𝗿𝗲](https://t.me/{BOT_USERNAME}?startgroup=true) 𝘁𝗼 𝗮𝗱𝗱 𝗺𝗲 𝘁𝗼 𝘆𝗼𝘂𝗿 𝗴𝗿𝗼𝘂𝗽!
 
-❔ 𝗙𝗼𝗿 information about all **feature** of this bot, 𝗷𝘂𝘀𝘁 𝘁𝘆𝗽𝗲 /help
+❔ 𝗙𝗼𝗿 𝗶𝗻𝗳𝗼𝗿𝗺𝗮𝘁𝗶𝗼𝗻 𝗮𝗯𝗼𝘂𝘁 𝗮𝗹𝗹 𝗳𝗲𝗮𝘁𝘂𝗿𝗲 𝗼𝗳 𝘁𝗵𝗶𝘀 𝗯𝗼𝘁, 𝗷𝘂𝘀𝘁 𝘁𝘆𝗽𝗲 /help
 </b>""",
 
         reply_markup=InlineKeyboardMarkup(
@@ -136,7 +136,7 @@ async def start(client: Client, message: Message):
     uptime_sec = (current_time - START_TIME).total_seconds()
     uptime = await _human_time_duration(int(uptime_sec))
     await message.reply_text(
-        f"""✅ **bot is running**\n<b>⚡ **uptime:**</b> `{uptime}`""",
+        f"""✅ **bot is running**\n<b>• **uptime:**</b> `{uptime}`\n• **start time:** `{START_TIME_ISO}`""",
         reply_markup=InlineKeyboardMarkup(
             [   
                 [    
@@ -159,7 +159,7 @@ async def start(client: Client, message: Message):
 )
 async def help(client: Client, message: Message):
     await message.reply_text(
-        """👋🏻**Hello** kamu, sebelum menggunakan bot kamu bisa membaca **Commands** dibawah ini.\n\n**Atau** kamu bisa langsung **menghubungi** creator apabila memerlukan **bantuan**""",
+        f"""👋🏻**Hello** {message.from_user.mention()}, sebelum menggunakan bot kamu bisa membaca **Commands** dibawah ini.\n\n**Atau** kamu bisa langsung **menghubungi** creator apabila memerlukan **bantuan**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
