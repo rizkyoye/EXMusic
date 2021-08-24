9e3e33b60f3962410c404fa7f329a236818606a0
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-25 01:24:16 +0700

    Update admins.py

--- EXmusic/modules/admins.py
+++ EXmusic/modules/admins.py
@@ -10,13 +10,15 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 
 from EXmusic.function.admins import admins
 from EXmusic.helpers.channelmusic import get_chat_id
-from EXmusic.helpers.decorators import authorized_users_only, errors
+from EXmusic.helpers.decorators import errors, authorized_users_only
 from EXmusic.helpers.filters import command, other_filters
 from EXmusic.services.callsmusic import callsmusic
 from EXmusic.services.callsmusic.callsmusic import queues
 from EXmusic.config import LOG_CHANNEL, OWNER, BOT_USERNAME
 from EXmusic.helpers.database import db, dcmdb, Database
 from EXmusic.helpers.dbtools import handle_user_status, delcmd_is_on, delcmd_on, delcmd_off
+from . import que, admins as fuck
+
 
 
 @Client.on_message()
