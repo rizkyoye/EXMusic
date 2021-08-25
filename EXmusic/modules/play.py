774a860fc5b8ef8045305a2a2313ae0d4e062106
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-26 04:45:43 +0700

    Update play.py

--- EXmusic/modules/play.py
+++ EXmusic/modules/play.py
@@ -38,7 +38,7 @@ from EXmusic.config import BOT_NAME as bn
 from EXmusic.config import DURATION_LIMIT
 from EXmusic.config import UPDATES_CHANNEL as updateschannel
 from EXmusic.config import que
-from EXmusic.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,BOT_USERNAME, OWNER
+from EXmusic.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,BOT_USERNAME, OWNER, KONTOL_IMG
 from EXmusic.function.admins import admins as a
 from EXmusic.helpers.admins import get_administrators
 from EXmusic.helpers.channelmusic import get_chat_id
@@ -640,7 +640,7 @@ async def play(_, message: Message):
                 ]
             ) 
             await message.reply_photo(
-                photo="https://telegra.ph/file/03b7244c8901a3f0c55d3.jpg",
+                photo=f"{KONTOL_IMG}",
                 caption=toxxt,
                 reply_markup=keyboard
             )
