85865d6aa6efa24a9ab5d7d24b060561675a6c12
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-26 03:32:30 +0700

    Update play.py

--- EXmusic/modules/play.py
+++ EXmusic/modules/play.py
@@ -139,10 +139,10 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
     Image.alpha_composite(image5, image6).save("temp.png")
     img = Image.open("temp.png")
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("etc/Roboto-Medium.ttf", 65)
-    draw.text((30, 570), f"PLAYING HERE", (0, 0, 0), font=font)
-    font = ImageFont.truetype("etc/Roboto-Regular.ttf", 50)
-    draw.text((30, 640),
+    font = ImageFont.truetype("etc/Quivira.otf", 65)
+    draw.text((25, 550), f"PLAYING HERE", (0, 0, 0), font=font)
+    font = ImageFont.truetype("etc/Roboto-Medium.ttf", 50)
+    draw.text((25, 620),
         f"{title}",
         (0, 0, 0),
         font=font,
