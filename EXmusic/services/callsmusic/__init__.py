24d1c77b94abecf161524a22fac033c1194b1133
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-25 23:22:17 +0700

    Update __init__.py

--- EXmusic/services/callsmusic/__init__.py
+++ EXmusic/services/callsmusic/__init__.py
@@ -1,5 +1,6 @@
 from pyrogram import Client
 from EXmusic import config
+from EXmusic.services.queues import queues
 
 client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
 run = client.run
