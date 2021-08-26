bce9724a2eebc164e5b3e8b777a1af3de42fab49
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-26 18:05:27 +0700

    Update play.py

--- EXmusic/modules/play.py
+++ EXmusic/modules/play.py
@@ -612,7 +612,7 @@ async def play(_, message: Message):
           await lel.edit("**Give the title of the song to play!**")
         # Looks like hell. Aren't it?? FUCK OFF
         try:
-            toxxt = "[🎛️](https://telegra.ph/file/492636b96c33b7f0c426a.jpg)"
+            toxxt = "[🎛️](https://telegra.ph/file/492636b96c33b7f0c426a.jpg)\n"
             toxxt = "💡 Please select the song you want to play :\n\n"
             j = 0
             useer=user_name
