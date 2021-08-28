a424a089b9977785f958eb1667d16e90a52dfadb
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-28 14:27:48 +0700

    Update callsmusic.py

--- EXmusic/services/callsmusic/callsmusic.py
+++ EXmusic/services/callsmusic/callsmusic.py
@@ -1,39 +1,23 @@
-# Calls Music 1 - Telegram bot for streaming audio in group calls
-# Copyright (C) 2021  Roj Serbest
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-
 from pyrogram import Client
 from pytgcalls import PyTgCalls
-from pytgcalls import Update
 
-from EXmusic.config import API_HASH, API_ID, SESSION_NAME
-from EXmusic.services.queues import queues
+from EXmusic.config import SESSION_NAME, API_ID, API_HASH
+from EXmusic.services.queues.queues import queues
 
 client = Client(SESSION_NAME, API_ID, API_HASH)
 pytgcalls = PyTgCalls(client)
 
 
 @pytgcalls.on_stream_end()
-async def on_stream_end(client: PyTgCalls, update: Update, chat_id: int) -> None:
+def on_stream_end(chat_id: int) -> None:
     queues.task_done(chat_id)
 
     if queues.is_empty(chat_id):
-        await pytgcalls.leave_group_call(chat_id)
+        pytgcalls.leave_group_call(chat_id)
     else:
-        await pytgcalls.change_stream({chat_id}, queues.get(chat_id)["file"])
+        pytgcalls.change_stream(
+            chat_id, queues.get(chat_id)["file"]
+        )
 
 
-run = pytgcalls.start()
+run = pytgcalls.run
