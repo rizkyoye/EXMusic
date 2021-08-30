95acff69b508b672b5410efded01f856a69f644c
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-08-31 05:39:10 +0700

    Update play.py

--- EXmusic/modules/play.py
+++ EXmusic/modules/play.py
@@ -29,7 +29,7 @@ from PIL import Image, ImageDraw, ImageFont
 from pyrogram import Client, filters
 from pyrogram.types import Voice
 from pyrogram.errors import UserAlreadyParticipant
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, Chat, CallbackQuery, ReplyKeyboardMarkup, KeyboardButtonCallback
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, Chat, CallbackQuery, ReplyKeyboardMarkup
 from Python_ARQ import ARQ
 from youtube_search import YoutubeSearch
 
