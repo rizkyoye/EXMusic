910008741f3dcb3520295f0613d88ab1c5559f6d
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-09-03 02:58:34 +0700

    bujed

--- EXmusic/modules/chat_member_updated.py
+++ EXmusic/modules/chat_member_updated.py
@@ -1,7 +1,7 @@
 from pyrogram import Client
 from pyrogram.types import ChatMemberUpdated
 
-from EXmusic.function import *
+from EXmusic.function import admins
 
 
 @Client.on_chat_member_updated()
